f5d18ee8889a943762a2d5ce1d933b050e93cf77
EvanDavidMS <evankund@gmail.com>
2022-12-11 00:40:59 -0600

    version de pagina v.2

--- funciones.js
+++ funciones.js
@@ -5,6 +5,8 @@ const a1 = document.getElementById('a1');
 const a2 = document.getElementById('a2');
 const a3 = document.getElementById('a3');
 const header = document.getElementById('header');
+/* Menu Movil */
+
 
 
 
@@ -17,3 +19,7 @@ changeColor.onclick = function() {
     a3.classList.toggle('active');
     header.classList.toggle('active');
 }
+
+
+
+
